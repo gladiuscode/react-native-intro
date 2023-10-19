a98cd07b28206bd7d419b671875b7b6631b39dd8
gladiuscode <gladiusservices@protonmail.com>
2023-10-19 12:31:37 +0200

    feat: implement SettingsScreen in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
 import React from "react";
 import {View} from 'react-native';
 import ThemeProvider from "./src/providers/theme/theme.provider";
+import SettingsScreen from "./src/screens/settings/settings.screen";
 
 export default function App() {
   return (
     <View style={{ flex: 1, paddingTop: 50 }}>
       <ThemeProvider>
+        <SettingsScreen />
       </ThemeProvider>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
