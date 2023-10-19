a58dec4ced3ae0e2cb8c79a3fa8d9f849166e665
gladiuscode <gladiusservices@protonmail.com>
2023-10-19 12:30:30 +0200

    feat: create customButton

---
+++ src/components/customButton.component.tsx
@@ -0,0 +1,22 @@
+import React, {memo} from "react";
+import {Text, TouchableOpacity} from "react-native";
+
+interface Props {
+  id: string;
+  label: string;
+  disabled?: boolean;
+  onTouchPress: (id: string) => void;
+}
+
+const CustomButton = memo<Props>(({ id, label, disabled = false, onTouchPress }) => {
+
+  const onPress = () => onTouchPress(id);
+
+  return (
+    <TouchableOpacity disabled={disabled} onPress={onPress}>
+      <Text>{label}</Text>
+    </TouchableOpacity>
+  );
+})
+
+export default CustomButton;
